c177eb633ec03c8b3917f6dc9dc6a256e08eb642
Antonoff <35700168+memearchivarius@users.noreply.github.com>
2025-10-09 22:24:02 +0300

    Update extra.js
    removed my TODO

--- extra.js
+++ extra.js
@@ -1,5 +1,3 @@
 /* See: https://mintlify.com/docs/settings/custom-scripts#custom-javascript */
 
 /* TODO: try to add a heading outline on mobile similar to one Astro provides */
-
-/* TODO: make sidebar group headers open their index page when clicked. */
