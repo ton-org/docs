4a62f2e3d4650be533eec494e263cd89a69738ac
Novus Nota <68142933+novusnota@users.noreply.github.com>
2025-12-01 01:16:01 +0100

    JavaScript!

--- .github/scripts/common.mjs
+++ .github/scripts/common.mjs
@@ -1,9 +1,11 @@
-export default async function({ github, context, exec }) {
-  const success = JSON.parse(process.env.SUCCESS ?? 'false');
-  const rawCommentText = process.env.COMMENT ?? '';
-  if (!success && rawCommentText === '') {
-    process.exit(0);
-  }
+export async function hidePriorCommentsWithPrefix({
+  github, // injected by GitHub
+  context, // injected by GitHub
+  exec, // injected by GitHub
+  prefix = '',
+  resolved = true,
+  user = 'github-actions[bot]',
+}) {
   const comments = await github.rest.issues.listComments({
     owner: context.repo.owner,
     repo: context.repo.repo,
@@ -21,11 +23,9 @@ export default async function({ github, context, exec }) {
     `, { nodeId: comment.node_id }))?.node?.isMinimized;
     await exec.exec('sleep 0.5s');
     if (isHidden) { continue; }
-    const body = comment.body;
-    const prefix = rawCommentText.slice(1, 30);
     if (
-      comment.user.login === 'github-actions[bot]' &&
-      body.startsWith(prefix)
+      comment.user.login === user &&
+      comment.body.startsWith(prefix)
     ) {
       console.log('Comment node_id:', comment.node_id);
       console.log(await github.graphql(`
@@ -42,17 +42,23 @@ export default async function({ github, context, exec }) {
         }
       `, {
         subjectId: comment.node_id,
-        classifier: success ? 'RESOLVED' : 'OUTDATED',
+        classifier: resolved ? 'RESOLVED' : 'OUTDATED',
       }));
       await exec.exec('sleep 0.5s');
     }
   }
-  if (!success) {
-    await github.rest.issues.createComment({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      issue_number: context.issue.number,
-      body: `${rawCommentText.slice(1, -1).replace(/\\n/g, '\n')}`,
-    });
-  }
+}
+
+export async function createComment({
+  github, // injected by GitHub
+  context, // injected by GitHub
+  exec, // injected by GitHub
+  body = '',
+}) {
+  await github.rest.issues.createComment({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.issue.number,
+    body: body,
+  });
 }
